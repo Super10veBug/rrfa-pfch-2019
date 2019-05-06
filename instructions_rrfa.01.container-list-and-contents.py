20c0013aab5b3ac16cd8bba2da11f56a195d0a58
Meghan <mlyon8@gmail.com>
2019-05-06 14:52:02 -0400

    Update instructions_rrfa.01.container-list-and-contents.py
    Create basic instructions to the RRFA.01 Test code

--- instructions_rrfa.01.container-list-and-contents.py
+++ instructions_rrfa.01.container-list-and-contents.py
@@ -1,14 +1,18 @@
+# import the ElementTree, Regex, and CSV modules. I imported pprint to more easily view the result_list in Terminal
 import xml.etree.ElementTree as etree
 import re
 import csv
 import pprint
 
-ElementTree = etree.parse('RRFA.01.TEST_ead.xml')
+# parse through the xml document -- 
 
+# the file name for the xml document you want to parse goes inside of the parentheses
+ElementTree = etree.parse('RRFA.01.TEST_ead.xml')
+# look for the root element,which should be EAD
 root = ElementTree.getroot()
 
+# and then loop through the elements to identify children of the root.
 # print(root)
-# 
 # for a in root:
 # 	print(a)
 # 	print('--------------------')
@@ -22,22 +26,28 @@ root = ElementTree.getroot()
 # 				print(d)
 # 				print('--------------------')
 
+# this is preparing the results of the .findall expression to be returned in a list
 result_list = []
+# here we are naming the csv columns and choosing the name for the csv file
 csv_columns = ['container', 'unittitle']
 csv_file = 'rrfa01_container-title_data.csv'
 
+# we are going to build a python dictionary using the results of the .findall regular expression
 for did_headings in root.findall(".//{urn:isbn:1-931666-22-9}did"):
 	dictionary = {
 	"container" : ""
 	}
-
+# here we are locating all of the text data for the <container> fields which are nested under the <did> fields
 	for containers in did_headings.findall(".//{urn:isbn:1-931666-22-9}container"):
 		dictionary['container'] = dictionary['container'] + containers.text
 	for unittitles in did_headings.findall(".//{urn:isbn:1-931666-22-9}unittitle"):
 		dictionary['unittitle'] = unittitles.text
+	# and appending the results into the dictionary as outlined above
 	result_list.append(dictionary)
+	# you should see a python dictionary when you run this script in the Terminal
 	print(result_list)
 
+# here the code for writing the python dictionary to a csv file
 with open(csv_file, 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
     writer.writeheader()
